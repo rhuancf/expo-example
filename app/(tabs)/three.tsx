06a49912e5d0416b4af028ec994e005f088b2d05
Rhuan Freire <rhuan.freire@rbmweb.com.br>
2024-07-15 18:14:27 -0300

    feat: backbutton block test

--- app/(tabs)/three.tsx
+++ app/(tabs)/three.tsx
@@ -1,13 +1,39 @@
-import { StyleSheet } from 'react-native';
+import { useFocusEffect } from "@react-navigation/native";
+import React, { useCallback } from "react";
+import { Alert, BackHandler, StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
 
 export default function TabThreeScreen() {
+  // BackHandler.addEventListener('hardwareBackPress', function() {return true});
+  useFocusEffect(
+    useCallback(() => {
+      const onBackPress = () => {
+        Alert.alert(
+          "Calma ae!",
+          "Você não pode usar o botão voltar nesta tela.",
+          [{ text: "OK", onPress: () => null }]
+        );
+        return true;
+      };
+
+      BackHandler.addEventListener("hardwareBackPress", onBackPress);
+
+      return () => {
+        BackHandler.removeEventListener("hardwareBackPress", onBackPress);
+      };
+    }, [])
+  );
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Three</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/three.tsx" />
     </View>
   );
@@ -16,16 +42,16 @@ export default function TabThreeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
